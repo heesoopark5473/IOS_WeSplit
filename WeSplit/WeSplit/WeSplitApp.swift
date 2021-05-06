dbff797d159dc50a07f551fb98ad3fdb6f3a6fd3
hep656 <hep656@usask.ca>
2021-05-06 13:38:44 -0600

    wesplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Heesoo Park on 2021-05-06.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
